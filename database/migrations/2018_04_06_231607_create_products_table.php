2d43871beb6b01e5e1192e3e27e42e920e14706d
x25-fer <x25.fer@gmail.com>
2018-04-07 15:49:51 -0300

    Migraciones de tablas products y reviews

--- database/migrations/2018_04_06_231607_create_products_table.php
+++ database/migrations/2018_04_06_231607_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name',150);
+            $table->text('detail');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
